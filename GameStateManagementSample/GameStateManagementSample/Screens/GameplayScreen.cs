6d5821a06fce2b67435dd85ff857db243cc848d4
Kenny <noobyken85@hotmail.com>
2014-12-24 22:56:00 +0800

    First workable

---
+++ GameStateManagementSample/GameStateManagementSample/Screens/GameplayScreen.cs
@@ -0,0 +1,333 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// GameplayScreen.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using System.Threading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using GameStateManagement;
+using ChaseCameraSample;
+#endregion
+
+namespace GameStateManagementSample
+{
+    /// <summary>
+    /// This screen implements the actual game logic. It is just a
+    /// placeholder to get the idea across: you'll probably want to
+    /// put some more interesting gameplay in here!
+    /// </summary>
+    class GameplayScreen : GameScreen
+    {
+        #region Fields
+
+        ContentManager content;
+        SpriteFont gameFont;
+
+        KeyboardState lastKeyboardState = new KeyboardState();
+        GamePadState lastGamePadState = new GamePadState();
+        MouseState lastMousState = new MouseState();
+        KeyboardState currentKeyboardState = new KeyboardState();
+        GamePadState currentGamePadState = new GamePadState();
+        MouseState currentMouseState = new MouseState();
+
+        Random random = new Random();
+
+        float pauseAlpha;
+
+        InputAction pauseAction;
+
+        Ship ship;
+        ChaseCamera camera;
+
+        Model shipModel;
+        Model groundModel;
+        Model cubeModel;
+
+        bool cameraSpringEnabled = true;
+
+        #endregion
+
+        #region Initialization
+
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public GameplayScreen()
+        {
+            TransitionOnTime = TimeSpan.FromSeconds(1.5);
+            TransitionOffTime = TimeSpan.FromSeconds(0.5);
+
+            pauseAction = new InputAction(
+                new Buttons[] { Buttons.Start, Buttons.Back },
+                new Keys[] { Keys.Escape },
+                true);
+
+            // Create the chase camera
+            camera = new ChaseCamera();
+
+            // Set the camera offsets
+            camera.DesiredPositionOffset = new Vector3(0.0f, 2000.0f, 3500.0f);
+            camera.LookAtOffset = new Vector3(0.0f, 150.0f, 0.0f);
+
+            // Set camera perspective
+            camera.NearPlaneDistance = 10.0f;
+            camera.FarPlaneDistance = 100000.0f;
+        }
+
+
+        /// <summary>
+        /// Load graphics content for the game.
+        /// </summary>
+        public override void Activate(bool instancePreserved)
+        {
+            if (!instancePreserved)
+            {
+                if (content == null)
+                    content = new ContentManager(ScreenManager.Game.Services, "Content");
+
+                gameFont = content.Load<SpriteFont>("gamefont");
+
+                shipModel = content.Load<Model>("Ship");
+                groundModel = content.Load<Model>("Ground");
+                cubeModel = content.Load<Model>("cube");
+
+                // Create ship
+                ship = new Ship(ScreenManager.GraphicsDevice);
+
+                camera.AspectRatio = (float)ScreenManager.GraphicsDevice.Viewport.Width /
+                    ScreenManager.GraphicsDevice.Viewport.Height;
+
+                UpdateCameraChaseTarget();
+                camera.Reset();
+
+                // A real game would probably have more content than this sample, so
+                // it would take longer to load. We simulate that by delaying for a
+                // while, giving you a chance to admire the beautiful loading screen.
+                Thread.Sleep(1000);
+
+                // once the load has finished, we use ResetElapsedTime to tell the game's
+                // timing mechanism that we have just finished a very long frame, and that
+                // it should not try to catch up.
+                ScreenManager.Game.ResetElapsedTime();
+            }
+
+#if WINDOWS_PHONE
+            if (Microsoft.Phone.Shell.PhoneApplicationService.Current.State.ContainsKey("PlayerPosition"))
+            {
+                playerPosition = (Vector2)Microsoft.Phone.Shell.PhoneApplicationService.Current.State["PlayerPosition"];
+                enemyPosition = (Vector2)Microsoft.Phone.Shell.PhoneApplicationService.Current.State["EnemyPosition"];
+            }
+#endif
+        }
+
+
+        public override void Deactivate()
+        {
+#if WINDOWS_PHONE
+            Microsoft.Phone.Shell.PhoneApplicationService.Current.State["PlayerPosition"] = playerPosition;
+            Microsoft.Phone.Shell.PhoneApplicationService.Current.State["EnemyPosition"] = enemyPosition;
+#endif
+
+            base.Deactivate();
+        }
+
+
+        /// <summary>
+        /// Unload graphics content used by the game.
+        /// </summary>
+        public override void Unload()
+        {
+            content.Unload();
+
+#if WINDOWS_PHONE
+            Microsoft.Phone.Shell.PhoneApplicationService.Current.State.Remove("PlayerPosition");
+            Microsoft.Phone.Shell.PhoneApplicationService.Current.State.Remove("EnemyPosition");
+#endif
+        }
+
+
+        #endregion
+
+        #region Update and Draw
+
+
+        /// <summary>
+        /// Updates the state of the game. This method checks the GameScreen.IsActive
+        /// property, so the game will stop updating when the pause menu is active,
+        /// or if you tab away to a different application.
+        /// </summary>
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus,
+                                                       bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, false);
+
+            // Gradually fade in or out depending on whether we are covered by the pause screen.
+            if (coveredByOtherScreen)
+                pauseAlpha = Math.Min(pauseAlpha + 1f / 32, 1);
+            else
+                pauseAlpha = Math.Max(pauseAlpha - 1f / 32, 0);
+
+            if (IsActive)
+            {
+                lastKeyboardState = currentKeyboardState;
+                lastGamePadState = currentGamePadState;
+                lastMousState = currentMouseState;
+
+#if WINDOWS_PHONE
+            currentKeyboardState = new KeyboardState();
+#else
+                currentKeyboardState = Keyboard.GetState();
+#endif
+                currentGamePadState = GamePad.GetState(PlayerIndex.One);
+                currentMouseState = Mouse.GetState();
+
+                bool touchTopLeft = currentMouseState.LeftButton == ButtonState.Pressed &&
+                        lastMousState.LeftButton != ButtonState.Pressed &&
+                        currentMouseState.X < ScreenManager.GraphicsDevice.Viewport.Width / 10 &&
+                        currentMouseState.Y < ScreenManager.GraphicsDevice.Viewport.Height / 10;
+
+
+                // Pressing the A button or key toggles the spring behavior on and off
+                if (lastKeyboardState.IsKeyUp(Keys.A) &&
+                    (currentKeyboardState.IsKeyDown(Keys.A)) ||
+                    (lastGamePadState.Buttons.A == ButtonState.Released &&
+                    currentGamePadState.Buttons.A == ButtonState.Pressed) ||
+                    touchTopLeft)
+                {
+                    cameraSpringEnabled = !cameraSpringEnabled;
+                }
+
+                // Reset the ship on R key or right thumb stick clicked
+                if (currentKeyboardState.IsKeyDown(Keys.R) ||
+                    currentGamePadState.Buttons.RightStick == ButtonState.Pressed)
+                {
+                    ship.Reset();
+                    camera.Reset();
+                }
+
+                // Update the ship
+                ship.Update(gameTime, shipModel, cubeModel);
+
+                // Update the camera to chase the new target
+                UpdateCameraChaseTarget();
+
+                // The chase camera's update behavior is the springs, but we can
+                // use the Reset method to have a locked, spring-less camera
+                if (cameraSpringEnabled)
+                    camera.Update(gameTime);
+                else
+                    camera.Reset();
+            }
+        }
+
+
+        /// <summary>
+        /// Lets the game respond to player input. Unlike the Update method,
+        /// this will only be called when the gameplay screen is active.
+        /// </summary>
+        public override void HandleInput(GameTime gameTime, InputState input)
+        {
+            if (input == null)
+                throw new ArgumentNullException("input");
+
+            // Look up inputs for the active player profile.
+            int playerIndex = (int)ControllingPlayer.Value;
+
+            KeyboardState keyboardState = input.CurrentKeyboardStates[playerIndex];
+            GamePadState gamePadState = input.CurrentGamePadStates[playerIndex];
+
+            // The game pauses either if the user presses the pause button, or if
+            // they unplug the active gamepad. This requires us to keep track of
+            // whether a gamepad was ever plugged in, because we don't want to pause
+            // on PC if they are playing with a keyboard and have no gamepad at all!
+            bool gamePadDisconnected = !gamePadState.IsConnected &&
+                                       input.GamePadWasConnected[playerIndex];
+
+            PlayerIndex player;
+            if (pauseAction.Evaluate(input, ControllingPlayer, out player) || gamePadDisconnected)
+            {
+#if WINDOWS_PHONE
+                ScreenManager.AddScreen(new PhonePauseScreen(), ControllingPlayer);
+#else
+                ScreenManager.AddScreen(new PauseMenuScreen(), ControllingPlayer);
+#endif
+            }
+            else
+            {
+
+            }
+        }
+
+        private void UpdateCameraChaseTarget()
+        {
+            camera.ChasePosition = ship.Position;
+            camera.ChaseDirection = ship.Direction;
+            camera.Up = ship.Up;
+        }
+
+
+        /// <summary>
+        /// Draws the gameplay screen.
+        /// </summary>
+        public override void Draw(GameTime gameTime)
+        {
+            // This game has a blue background. Why? Because!
+            ScreenManager.GraphicsDevice.Clear(ClearOptions.Target,
+                                               Color.CornflowerBlue, 0, 0);
+
+            // Our player and enemy are both actually just text strings.
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+
+            ScreenManager.GraphicsDevice.BlendState = BlendState.Opaque;
+            ScreenManager.GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+            ScreenManager.GraphicsDevice.SamplerStates[0] = SamplerState.LinearWrap;
+
+            DrawModel(shipModel, ship.World);
+            DrawModel(groundModel, Matrix.Identity);
+            DrawModel(cubeModel, Matrix.CreateTranslation(50, 50, 0) * Matrix.CreateScale(100));
+
+            spriteBatch.Begin();
+
+            spriteBatch.End();
+
+            // If the game is transitioning on or off, fade it out to black.
+            if (TransitionPosition > 0 || pauseAlpha > 0)
+            {
+                float alpha = MathHelper.Lerp(1f - TransitionAlpha, 1f, pauseAlpha / 2);
+
+                ScreenManager.FadeBackBufferToBlack(alpha);
+            }
+        }
+
+        private void DrawModel(Model model, Matrix world)
+        {
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.World = transforms[mesh.ParentBone.Index] * world;
+
+                    // Use the matrices provided by the chase camera
+                    effect.View = camera.View;
+                    effect.Projection = camera.Projection;
+                }
+                mesh.Draw();
+            }
+        }
+        #endregion
+    }
+}
